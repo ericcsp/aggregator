d2e19cacbae53d7bc2099563b51b1bfd2b6a96c4
Eric Stofferahn <eric@csp-inc.org>
2021-05-24 21:24:13 +0000

    update

--- aggregator.py
+++ aggregator.py
@@ -6,9 +6,10 @@ class Stats:
         self.shapefile = shapefile
         self.tifname = tifname 
         self.statsfile = statsfile
+        self.mystats = ["std", "count", "mean", "median", "percentile_10", "percentile_90", "min", "max", "sum"]
 
     def make_vector_summary(self):
-        self.stats = zonal_stats(self.shapefile, self.tifname, geojson_out=True)
+        self.stats = zonal_stats(self.shapefile, self.tifname, stats=self.mystats, geojson_out=True)
         with open(self.statsfile, 'w') as fh:
             geojson.dump(self.stats, fh)
 
@@ -16,9 +17,9 @@ class Stats:
         self.make_vector_summary()
 
 def main(input):
-    tifname = f'{input}_remapped.tif'
-    shapefile = '/data/placer/shp/AOI'
-    statsfile = f'{input}_stats'
+    tifname = f'/home/cspadmin/data/{input}.tif'
+    shapefile = '/home/cspadmin/data/PLACER_fire_bounds_2016_2018.geojson'
+    statsfile = f'{input}_stats.geojson'
     my_stats = Stats(shapefile, tifname, statsfile) 
     my_stats.main()
     return my_stats
